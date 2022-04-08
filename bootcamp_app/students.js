d86778611b7c18e3a774085014f64bc0e2a24663
‘Gonzo <gonzonieto@gmail.com>
2022-04-08 02:45:55 +0000

    parameterize SQL query to prevent SQL injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,30 +9,27 @@ const pool = new Pool({
 });
 
 console.log();
-if (args.length) {
+if (args.length > 2) {
   console.table(args);
 }
 
 const cohort = process.argv[2];
 const limit = process.argv[3];
 
-pool.query(`
-SELECT s.id,
-       s.name,
-       c.name AS cohort
-  FROM students AS s
-  JOIN cohorts AS c ON s.cohort_id = c.id
- WHERE c.name ILIKE '${cohort}%'  
- LIMIT ${limit || 5};
-`)
+const query = {
+  name: 'students-in-cohort',
+  text: `SELECT s.id, s.name, c.name AS cohort
+           FROM students AS s
+           JOIN cohorts AS c ON s.cohort_id = c.id
+          WHERE c.name ILIKE $1
+          LIMIT $2;`,
+  values: [`${cohort}%`, limit],
+};
+
+pool
+  .query(query)
   .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-    });
+    res.rows.forEach(user => console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`));
   })
-  .catch(err => {
-    console.error('query error', err.stack);
-  })
-  .finally(() => {
-    pool.end();
-  });
+  .catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,24 +15,24 @@ if (process.argv.length > 2) {
 
 console.log();
 
-const cohort = process.argv[2];
+const cohort = process.argv[2] || 'JUL02';
 
-pool.query(`
-SELECT t.name AS teacher, c.name AS cohort, COUNT(ar.*)
-  FROM assistance_requests AS ar
-  JOIN students AS s ON s.id = ar.student_id
-  JOIN teachers AS t ON t.id = ar.teacher_id
-  JOIN cohorts AS c ON c.id = s.cohort_id
- WHERE c.name = '${cohort || 'JUL02'}'
- GROUP BY cohort, teacher
- ORDER BY t.name;
-`)
+const query = {
+  name: 'students-in-cohort',
+  text: `SELECT t.name AS teacher, c.name AS cohort, COUNT(ar.*)
+           FROM assistance_requests AS ar
+           JOIN students AS s ON s.id = ar.student_id
+           JOIN teachers AS t ON t.id = ar.teacher_id
+           JOIN cohorts AS c ON c.id = s.cohort_id
+          WHERE c.name = $1
+          GROUP BY cohort, teacher
+          ORDER BY t.name;`,
+  values: [`${cohort}`],
+};
+
+pool.query(query)
   .then(res => {
     console.log(res.rows);
   })
-  .catch(err => {
-    console.error('query error', err.stack);
-  })
-  .finally(() => {
-    pool.end();
-  });
+  .catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());
